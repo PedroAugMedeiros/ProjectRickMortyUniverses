a66882d4d0d5510c5504c65f9b23df21ad95006b
Pedro Augusto Godoi Medeiros <87546291+PedroAugMedeiros@users.noreply.github.com>
2022-06-30 11:02:38 -0300

    start: install styled-components lib and types dependencies

---
+++ src/App.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    padding-bottom: 5%;
+    background-color: #FFF;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    width: 100vw;
+`;
+
+export const Body = styled.div`
+  width: 100vw;
+  text-align: center;
+`;
